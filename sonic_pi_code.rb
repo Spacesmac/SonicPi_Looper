6fd7a96076f2021e1b118c507450b9f46bb4a84e
Clement <clement.touanen@epitech.eu>
2024-06-06 17:47:47 +0100

    [ADD] - receiving generated code and playing it in Sonic Pi

--- sonic_pi_code.rb
+++ sonic_pi_code.rb
@@ -1,14 +1,19 @@
 live_loop :drum_pad do
-    use_real_time
-    sample_name = sync "/osc*/drum_pad"
-    case sample_name[0]
-    when 'bd_haus'
-      sample :bd_haus
-    when 'sn_dub'
-      sample :sn_dub
-    when 'elec_hi_snare'
-      sample :elec_hi_snare
-    when 'drum_cymbal_closed'
-      sample :drum_cymbal_closed
-    end
+  use_real_time
+  sample_name = sync "/osc*/drum_pad"
+  case sample_name[0]
+  when 'bd_haus'
+    sample :bd_haus
+  when 'sn_dub'
+    sample :sn_dub
+  when 'elec_hi_snare'
+    sample :elec_hi_snare
+  when 'drum_cymbal_closed'
+    sample :drum_cymbal_closed
   end
+end
+
+live_loop :code_receiver do
+  code = sync "/osc*/run-code"
+  eval code[0]
+end

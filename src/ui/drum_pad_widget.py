769d4cba505ba9f6b73ddd671caf4218816cd405
Clement <clement.touanen@epitech.eu>
2024-07-19 14:38:31 +0200

    [ADD] - new selection for instruments and live play with keyboard to fill the grid(still need some adjustement)

--- src/ui/drum_pad_widget.py
+++ src/ui/drum_pad_widget.py
@@ -1,5 +1,9 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QPushButton, QLabel, QLineEdit
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import (
+    QWidget, QVBoxLayout, QGridLayout, QPushButton, QLabel,
+    QLineEdit, QComboBox, QMenu, QAction, QToolButton,
+)
+from functools import partial
+from PyQt5.QtCore import Qt, QTimer, QSize
 from PyQt5.QtGui import QIntValidator
 from logic.osc_client import send_osc_message
 from logic.recording import add_recorded_event
@@ -11,6 +15,22 @@ class DrumPadWidget(QWidget):
         self.cols_max = 8
         self.cols_loop_speed = 4
         self.playing_column = 0
+        self.is_recording = False
+        self.drum_samples = {
+            'Kicks:': ['drum_heavy_kick'],
+            'Bass Drums': ['drum_bass_soft', 'drum_bass_hard', 'bd_808', 'bd_chip', 'bd_mehackit', 'bd_zum', 'bd_ada', 'bd_boom', 'bd_fat', 'bd_gas', 'bd_haus', 'bd_klub', 'bd_pure', 'bd_sone', 'bd_tek', 'bd_zome'],
+            'Snare Drums': ['drum_snare_hard', 'drum_snare_soft', 'sn_dub', 'sn_dolf', 'sn_zome', 'sn_generic'],
+            'Tom Drums': ['drum_tom_hi_hard', 'drum_tom_hi_soft', 'drum_tom_lo_hard', 'drum_tom_lo_soft', 'drum_tom_mid_hard', 'drum_tom_mid_soft'],
+            'Cymbals': ['drum_cymbal_closed', 'drum_cymbal_hard', 'drum_cymbal_open', 'drum_cymbal_pedal', 'drum_cymbal_soft'],
+            'Hi-hats': ['hat_bdu', 'hat_cab', 'hat_cats'],
+            'Other Percussions': ['drum_cowbell', 'drum_roll', 'elec_blip', 'elec_blip2', 'elec_bong', 'elec_chime', 'elec_filt_snare', 'elec_flip', 'elec_fuzz_tom', 'elec_hollow_kick', 'elec_lo_snare', 'elec_mid_snare', 'elec_ping', 'elec_pop', 'elec_snare', 'elec_soft_kick', 'elec_twang', 'elec_wood'],
+        }
+        self.key_sample_map = {
+            'a': 'drum_heavy_kick',
+            's': 'drum_snare_hard',
+            'd': 'hat_bdu',
+            'f': 'drum_cymbal_closed'
+        }
         self.initUI()
 
     def initUI(self):
@@ -37,7 +57,7 @@ class DrumPadWidget(QWidget):
         self.controls_layout.addWidget(self.pad_speed, 0, 3)
 
         self.pad_number_label = QLabel("Pads Number:")
-        self.pad_number_label.setFixedWidth(70)
+        self.pad_number_label.setFixedWidth(80)
         self.controls_layout.addWidget(self.pad_number_label, 0, 4)
         self.pad_number = QLineEdit(str(self.cols_max))
         self.pad_number.setValidator(QIntValidator(2, 16))
@@ -47,15 +67,20 @@ class DrumPadWidget(QWidget):
 
         play_button = QPushButton("Play")
         play_button.clicked.connect(self.start_metronome)
-        self.controls_layout.addWidget(play_button, 0, self.cols_max - 2)
+        self.controls_layout.addWidget(play_button, 0, 6)
 
         stop_button = QPushButton("Stop")
         stop_button.clicked.connect(self.stop_metronome)
-        self.controls_layout.addWidget(stop_button, 0, self.cols_max - 1)
+        self.controls_layout.addWidget(stop_button, 0,7)
+
+        rec_button = QPushButton("Rec")
+        rec_button.setCheckable(True)
+        rec_button.clicked.connect(self.toggle_recording)
+        self.controls_layout.addWidget(rec_button, 0, 8)
 
         generate_button = QPushButton("Generate Code")
         generate_button.clicked.connect(self.generate_code_from_grid)
-        self.controls_layout.addWidget(generate_button, 0, self.cols_max)
+        self.controls_layout.addWidget(generate_button, 0, 9)
 
         # Add controls layout to main layout
         self.main_layout.addLayout(self.controls_layout)
@@ -83,24 +108,30 @@ class DrumPadWidget(QWidget):
             self.indicator_lines[col].setStyleSheet("background-color: red;")
             self.indicator_lines[col].setFixedHeight(20)
             self.grid_layout.addWidget(self.indicator_lines[col], 0, col + 1)
-
-        self.key_sample_map = {
-            'a': 'bd_haus',
-            's': 'sn_dub',
-            'd': 'elec_hi_snare',
-            'f': 'drum_cymbal_closed'
-        }
         self.rows_max = len(self.key_sample_map)
         self.sequence_grid = [[False for _ in range(self.cols_max)] for _ in range(self.rows_max)]
-
+        button_size = QSize(250, 30)
         self.interval_per_pad = 60 / (self.bpm * self.cols_loop_speed) * 1000
 
-        # Create buttons for pads
+        # Create buttons for pads and dropdown menus for instruments
         for row, (key, sample) in enumerate(self.key_sample_map.items()):
-            name_button = QPushButton(f"Play {sample} (Key: {key.upper()})")
-            name_button.clicked.connect(lambda _, s=sample: self.on_name_button_press(s))
+            name_button = QToolButton()
+            name_button.setText(f"Play {sample} (Key: {key.upper()})")
             name_button.setStyleSheet("background-color: lightgrey")
+            name_button.setFixedSize(button_size)
             self.grid_layout.addWidget(name_button, row + 1, 0)
+
+            # Create dropdown menu for instrument selection
+            menu = QMenu()
+            for category, instruments in self.drum_samples.items():
+                category_menu = QMenu(category, self)
+                for instrument in instruments:
+                    action = QAction(instrument, self)
+                    action.triggered.connect(partial(self.change_sample, key, instrument, name_button))
+                    category_menu.addAction(action)
+                menu.addMenu(category_menu)
+            name_button.setMenu(menu)
+            name_button.setPopupMode(QToolButton.InstantPopup)
 
             for col in range(self.cols_max):
                 pad_button = QPushButton()
@@ -108,6 +139,9 @@ class DrumPadWidget(QWidget):
                 self.update_button_color(pad_button, self.sequence_grid[row][col])  # Set initial color based on sequence_grid
                 self.grid_layout.addWidget(pad_button, row + 1, col + 1)
 
+    def change_sample(self, key, sample, button):
+        self.key_sample_map[key] = sample
+        button.setText(f"Play {sample} (Key: {key.upper()})")
     def update_bpm_from_input(self):
         try:
             self.bpm = int(self.bpm_input.text())
@@ -134,6 +168,14 @@ class DrumPadWidget(QWidget):
         except ValueError:
             self.bpm_input.setText(str(self.bpm))
             self.pad_speed.setText(str(self.cols_loop_speed))
+
+    def toggle_recording(self):
+        self.is_recording = not self.is_recording
+        if self.is_recording:
+            self.start_metronome()  # Ensure the metronome is running when recording starts
+            print("Recording started")
+        else:
+            print("Recording stopped")
 
     def update_button_color(self, button, is_active):
         if is_active:
@@ -194,6 +236,7 @@ class DrumPadWidget(QWidget):
         print(sonic_pi_code)
 
     def play_next_column(self):
+        osc_list = []
         for col in range(self.cols_max):
             self.indicator_lines[col].setStyleSheet("background-color: red;")
         self.indicator_lines[self.playing_column].setStyleSheet("background-color: green;")
@@ -201,14 +244,23 @@ class DrumPadWidget(QWidget):
             if self.sequence_grid[row][self.playing_column]:
                 print("Playing cols")
                 sample = self.key_sample_map[list(self.key_sample_map.keys())[row]]
-                send_osc_message("/drum_pad", sample)
+                osc_list.append(sample)
+        if osc_list.__len__() > 0:
+            send_osc_message("/drum_pad", osc_list)
         self.playing_column = (self.playing_column + 1) % self.cols_max
 
     def handle_key_press(self, event):
         key = event.text()
         print(f"Key press event in DrumPadWidget: {key}")  # Debug print
         if key in self.key_sample_map:
-            self.on_name_button_press(self.key_sample_map[key])
+            sample = self.key_sample_map[key]
+            self.on_name_button_press(sample)
+            
+            if self.is_recording:
+                current_col = self.playing_column
+                row = list(self.key_sample_map.keys()).index(key)
+                self.sequence_grid[row][current_col] = True
+                self.update_button_color(self.grid_layout.itemAtPosition(row + 1, current_col + 1).widget(), True)
     
     def set_playing_column(self, column):
         self.indicator_line.setGeometry((column + 1) * 50, 0, 2, self.height())
